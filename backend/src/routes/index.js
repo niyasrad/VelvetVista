4056c177f5660599db53dc3026de4fb1e94256fb
niyasrad <niyasrad@gmail.com>
2023-09-09 02:30:40 +0530

    feat(db): added get requests, contacts to user docs

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,9 @@
 const userRoute = require('./user.routes')
+const contactRoute = require('./contact.routes')
 
 const combineRoutes = (app) => {
     app.use('/user', userRoute)
+    app.use('/contact', contactRoute)
 }
 
 module.exports = combineRoutes
